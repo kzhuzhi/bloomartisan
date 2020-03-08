60c43b2936abdbf520f1ca6b65371b20980a7efa
kzhuzhi <zhuzhi94@gmail.com>
2020-03-08 21:50:13 +0800

    Added Signatures component

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <Home />
+        <Signatures />
         <Menu />
         <Gallery />
         <Location />
