5007ac8826956c83e82de98552d5b23023c9e0ca
kzhuzhi <zhuzhi94@gmail.com>
2020-03-30 16:04:17 +0800

    - fixed mobile/responsive view - added specialties write-up

--- src/components/SignatureDishes/index.jsx
+++ src/components/SignatureDishes/index.jsx
@@ -9,25 +9,18 @@ class SignatureDishes extends Component {
                         <h2>our specials</h2>
                     </div>
                     <div className="row">
-                        <div className="col-xs-12 col-sm-4">
+                        <div className="col-xs-12 col-sm-6">
                             <div className="signatures-item">
-                                <h3>Lorem ipsum dolor</h3>
-                                <img src="img/specials/1.jpg" className="img-responsive" alt="" />
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam sed commodo.</p>
+                                <h3>Butter Milk Waffle</h3>
+                                <img src="img/gallery/2.jpg" className="img-responsive" alt="" />
+                                <p>Simple yet delicious homemade waffle made with real buttermilk, giving it that rich buttery flavour. Light and crispy on the outside, soft and tender on the inside. Pair it with a scoop of our homemade gelato for the best experience.</p>
                             </div>
                         </div>
-                        <div className="col-xs-12 col-sm-4">
+                        <div className="col-xs-12 col-sm-6">
                             <div className="signatures-item">
-                                <h3>Consectetur adipiscing</h3>
-                                <img src="img/specials/2.jpg" className="img-responsive" alt="" />
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam sed commodo.</p>
-                            </div>
-                        </div>
-                        <div className="col-xs-12 col-sm-4">
-                            <div className="signatures-item">
-                                <h3>Duis sed dapibus</h3>
-                                <img src="img/specials/3.jpg" className="img-responsive" alt="" />
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam sed commodo.</p>
+                                <h3>Gelato</h3>
+                                <img src="img/gallery/3.jpg" className="img-responsive" alt="" />
+                                <p>100% homemade gelato, made fresh everyday for your enjoyment. Our gelato comes in various curated flavours to suit all kinds of cravings that you have! Pair it with our homemade baked goods for that complete dessert experience.</p>
                             </div>
                         </div>
                     </div>
