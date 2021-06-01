96511888060dc3f51a7aa06aadd3959267686062
kzhuzhi <zhuzhi94@gmail.com>
2021-06-01 23:28:30 +0800

    commented prices away on menu

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -14,30 +14,30 @@ class Menu extends Component {
                                 <h3 className="menu-section-title">GELATO</h3>
                                 <div className="menu-item">
                                     <div className="menu-item-name">single classic</div>
-                                    <div className="menu-item-price"> 3.5 </div>
+                                    {/* <div className="menu-item-price"> 3.5 </div> */}
                                     <div className="menu-item-name">single premium</div>
-                                    <div className="menu-item-price"> 4.2 </div>
+                                    {/* <div className="menu-item-price"> 4.2 </div> */}
                                     <div className="menu-item-name">double classic</div>
-                                    <div className="menu-item-price"> 6 </div>
+                                    {/* <div className="menu-item-price"> 6 </div> */}
                                     <div className="menu-item-name">double premium</div>
-                                    <div className="menu-item-price"> 7.4 </div>
+                                    {/* <div className="menu-item-price"> 7.4 </div> */}
                                     <div className="menu-item-name">homemade waffle cone</div>
-                                    <div className="menu-item-price"> +0.7 </div>
+                                    {/* <div className="menu-item-price"> +0.7 </div> */}
                                     <br />
                                     <div className="menu-item-name">16oz classic pint</div>
-                                    <div className="menu-item-price"> 12</div>
+                                    {/* <div className="menu-item-price"> 12</div> */}
                                     <div className="menu-item-name">26oz classic pint</div>
-                                    <div className="menu-item-price"> 18 </div>
+                                    {/* <div className="menu-item-price"> 18 </div> */}
                                     <div className="menu-item-name">16oz premium pint</div>
-                                    <div className="menu-item-price"> 16 </div>
+                                    {/* <div className="menu-item-price"> 16 </div> */}
                                     <div className="menu-item-name">26oz premium pint</div>
-                                    <div className="menu-item-price"> 23 </div>
+                                    {/* <div className="menu-item-price"> 23 </div> */}
                                 </div>
                                 <br />
                                 <h3 className="menu-section-title">WAFFLE</h3>
                                 <div className="menu-item">
                                     <div className="menu-item-name">brown butter sourdough waffle</div>
-                                    <div className="menu-item-price"> 4 </div>
+                                    {/* <div className="menu-item-price"> 4 </div> */}
                                 </div>
                                 <br />
                                 <h3 className="menu-section-title">BAKES (subject to availability)</h3>
@@ -49,45 +49,45 @@ class Menu extends Component {
                                 <h3 className="menu-section-title">COFFEE</h3>
                                 <div className="menu-item">
                                     <div className="menu-item-name">espresso</div>
-                                    <div className="menu-item-price"> 3.5 </div>
+                                    {/* <div className="menu-item-price"> 3.5 </div> */}
                                     <div className="menu-item-name">americano</div>
-                                    <div className="menu-item-price"> 3.5 </div>
+                                    {/* <div className="menu-item-price"> 3.5 </div> */}
                                     <div className="menu-item-name">latte</div>
-                                    <div className="menu-item-price"> 4.5 </div>
+                                    {/* <div className="menu-item-price"> 4.5 </div> */}
                                     <div className="menu-item-name">cappuccino</div>
-                                    <div className="menu-item-price"> 5 </div>
+                                    {/* <div className="menu-item-price"> 5 </div> */}
                                     <div className="menu-item-name">affogato</div>
-                                    <div className="menu-item-price"> 6 </div>
+                                    {/* <div className="menu-item-price"> 6 </div> */}
                                     <div className="menu-item-name">iced</div>
-                                    <div className="menu-item-price"> +1 </div>
+                                    {/* <div className="menu-item-price"> +1 </div> */}
                                 </div>
                                 <br />
                                 <h3 className="menu-section-title">TEA</h3>
                                 <div className="menu-item">
                                     <div className="menu-item-name">english breakfast</div>
-                                    <div className="menu-item-price"> 4 </div>
+                                    {/* <div className="menu-item-price"> 4 </div> */}
                                     <div className="menu-item-name">earl grey</div>
-                                    <div className="menu-item-price"> 4 </div>
+                                    {/* <div className="menu-item-price"> 4 </div> */}
                                     <div className="menu-item-name">chamomile</div>
-                                    <div className="menu-item-price"> 4 </div>
+                                    {/* <div className="menu-item-price"> 4 </div> */}
                                     <div className="menu-item-name">peppermint</div>
-                                    <div className="menu-item-price"> 4 </div>
+                                    {/* <div className="menu-item-price"> 4 </div> */}
                                     <div className="menu-item-name">green tea</div>
-                                    <div className="menu-item-price"> 4 </div>
+                                    {/* <div className="menu-item-price"> 4 </div> */}
                                 </div>
                                 <br />
                                 <h3 className="menu-section-title">COOLER</h3>
                                 <div className="menu-item">
                                     <div className="menu-item-name">iced earl grey lavender</div>
-                                    <div className="menu-item-price"> 5 </div>
+                                    {/* <div className="menu-item-price"> 5 </div> */}
                                     <div className="menu-item-name">spring tempest</div>
-                                    <div className="menu-item-price"> 5 </div>
+                                    {/* <div className="menu-item-price"> 5 </div> */}
                                     <div className="menu-item-name">summer love</div>
-                                    <div className="menu-item-price"> 5 </div>
+                                    {/* <div className="menu-item-price"> 5 </div> */}
                                     <div className="menu-item-name">autumn breeze</div>
-                                    <div className="menu-item-price"> 5 </div>
+                                    {/* <div className="menu-item-price"> 5 </div> */}
                                     <div className="menu-item-name">winter rain</div>
-                                    <div className="menu-item-price"> 5 </div>
+                                    {/* <div className="menu-item-price"> 5 </div> */}
                                 </div>
                                 <br />
                             </div>
